dee152941b8fe79f11064dbfd3ba2c61f7895bd8
M S Akter <shahinurmony4@gmail.com>
2023-02-18 19:47:02 +0600

    validation added

--- js/common.js
+++ js/common.js
@@ -2,14 +2,14 @@
 function input(inputId) {
     const inputItem = document.getElementById(inputId);
     const inputFieldString = inputItem.value;
-    const inputField = parseInt(inputFieldString);
+    const inputField = parseFloat(inputFieldString);
     return inputField;
 }
 
 function get(elementId){
     const element = document.getElementById(elementId);
     const elementString = element.innerText;
-    const elementValue = parseInt(elementString);
+    const elementValue = parseFloat(elementString);
     return elementValue;  
 }
 
@@ -22,3 +22,18 @@ function setInput(id, number){
     const setDataField = document.getElementById(id);
     setDataField.value = number;
 }
+
+// common function to display result
+function displayData(id, result) {
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${id}</td>
+      <td>${result}</td>
+      <td>
+      <button class="btn btn-sm btn-blue-500">Convert-to-m2</button>
+      </td>
+    `;
+    container.appendChild(tr);
+  }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,95 @@
-document.getElementById('btn-blog').addEventListener('click',function () {
+document.getElementById('btn-blog').addEventListener('click', function () {
   window.location.href = 'my-blogs.html';
 })
 
-document.getElementById('')
+let serial = 0;
+document.getElementById('btn-triangle').addEventListener('click', function () {
+  serial += 1;
+  const triangleB = document.getElementById('field-b').value;
+  const triangleH = document.getElementById('field-h').value;
+  if (
+    triangleB == "" ||
+    triangleH == "" ||
+    triangleB <= 0 ||
+    triangleH <= 0 ||
+    isNaN(triangleB) ||
+    isNaN(triangleH)
+  ) {
+    return alert("please enter any valid number");
+  }
+
+  triangleAreaResult = 0.5 * triangleB * triangleH;
+  const triangle = parseFloat(triangleAreaResult.toFixed(2));
+  
+  console.log( triangle);
+})
+
+document.getElementById('btn-rectangle').addEventListener('click', function () {
+  serial += 1;
+  const rectangleW = document.getElementById('field-w').value;
+  const rectangleL = document.getElementById('field-l').value;
+
+  if (
+    rectangleW == "" ||
+    rectangleL == "" ||
+    rectangleW <= 0 ||
+    rectangleL <= 0 ||
+    isNaN(rectangleW) ||
+    isNaN(rectangleL)
+  ) {
+    return alert("please enter any valid number");
+  }
+
+  rectangleAreaResult = rectangleW * rectangleL;
+  const rectangle = parseFloat(rectangleAreaResult.toFixed(2));
+  
+  console.log( rectangle);
+  
+})
+
+document.getElementById('btn-parallelogram').addEventListener('click', function () {
+  serial += 1;
+  const parallelogramB = document.getElementById('parallelogram-b-text').innerText;
+  const parallelogramH = document.getElementById('parallelogram-h-text').innerText;
+
+  parallelogramAreaResult = parallelogramB * parallelogramH;
+  const parallelogram = parseFloat(parallelogramAreaResult.toFixed(2));
+  
+  console.log( parallelogram);
+})
+
+document.getElementById('btn-rhombus').addEventListener('click', function () {
+  serial += 1;
+  const rhombusD1 = document.getElementById('rhombus-d1-text').innerText;
+  const rhombusD2 = document.getElementById('rhombus-d2-text').innerText;
+
+  rhombusAreaResult = 0.5 * rhombusD1 * rhombusD2;
+  const rhombus = parseFloat(rhombusAreaResult.toFixed(2));
+  
+  console.log( rhombus);
+})
+
+document.getElementById('btn-pentagon').addEventListener('click', function () {
+  serial += 1;
+  const pentagonP = document.getElementById('pentagon-p-text').innerText;
+  const pentagonB = document.getElementById('pentagon-b-text').innerText;
+
+  pentagonAreaResult = 0.5 * pentagonP * pentagonB;
+  const pentagon = parseFloat(pentagonAreaResult.toFixed(2));
+  
+  console.log( pentagon);
+  
+})
+
+document.getElementById("btn-ellipse").addEventListener("click", function () {
+  serial += 1;
+  const ellipseA = document.getElementById('ellipse-a-text').innerText;
+  const ellipseB = document.getElementById('ellipse-b-text').innerText;
+
+  ellipseAreaResult = 3.14 * ellipseA * ellipseB;
+  const ellipse = parseFloat(ellipseAreaResult.toFixed(2));
+  
+  console.log( ellipse);
+
+});
+
